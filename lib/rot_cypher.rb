d841735b77f97b3d1eb45ed436f26c1dd6ecb3d3
Velimir Gvozdenac <vgvozdenac.vg@gmail.com>
2022-01-12 15:31:21 +0100

    Delete trailing spaces

--- lib/rot_cypher.rb
+++ lib/rot_cypher.rb
@@ -1,28 +1,28 @@
-class RotCypher 
+class RotCypher
   Letters_array = ("a".."z").to_a
   Upper_letters_array = ("A".."Z").to_a
 
   def encrypt(message_to_encrypt, shift_key)
     encrypted_message = ""
-    message_to_encrypt.split('').each do |letter_to_encrypt| 
+    message_to_encrypt.split('').each do |letter_to_encrypt|
       if(letter_to_encrypt == " ")
         encrypted_message += " "
-      else  
+      else
         if Upper_letters_array.include? letter_to_encrypt
-          index = if(Upper_letters_array.find_index(letter_to_encrypt)<26-shift_key)
+          index = if(Upper_letters_array.find_index(letter_to_encrypt) < 26 - shift_key)
                     (Upper_letters_array.find_index(letter_to_encrypt) + shift_key)
                   else
-                    (Upper_letters_array.find_index(letter_to_encrypt)-26+shift_key)
-                  end  
+                    (Upper_letters_array.find_index(letter_to_encrypt) - 26 + shift_key)
+                  end
           encrypted_message += Upper_letters_array[index]
         else
-          index = if(Letters_array.find_index(letter_to_encrypt)<26-shift_key)
+          index = if(Letters_array.find_index(letter_to_encrypt) < 26 - shift_key)
                     (Letters_array.find_index(letter_to_encrypt) + shift_key)
                   else
-                    (Letters_array.find_index(letter_to_encrypt)-26+shift_key)
-                  end 
+                    (Letters_array.find_index(letter_to_encrypt) - 26 + shift_key)
+                  end
           encrypted_message += Letters_array[index]
-        end 
+        end
       end
     end
     encrypted_message
--- spec/rot_cypher_spec.rb
+++ spec/rot_cypher_spec.rb
@@ -2,29 +2,29 @@ require "rot_cypher"
 describe RotCypher do
     let(:rc) {RotCypher.new}
 
-    context "Encrypt with shift key  5 with 'omg'" do
-        it "returns 'trl'" do
-            expect(rc.encrypt("omg",5)).to eq("trl")
-        end
-    end
-    context "Encrypt with shift key  0 with 'c'" do
-        it "returns 'c'" do
-            expect(rc.encrypt("c",0)).to eq("c")
-        end
-    end 
-    context "Encrypt with shift key  26 with 'Cool'" do
-        it "returns 'Cool'" do
-            expect(rc.encrypt("Cool",26)).to eq("Cool")
-        end
-    end
-    context "Encrypt with shift key  13 with 'The quick brown fox jumps over the lazy dog'" do
-        it "returns 'Gur dhvpx oebja sbk whzcf bire gur ynml qbt'" do
-            expect(rc.encrypt("The quick brown fox jumps over the lazy dog",13)).to eq("Gur dhvpx oebja sbk whzcf bire gur ynml qbt")
-        end
-    end
-    context "Encrypt with shift key  3 with 'Gur dhvpx oebja sbk whzcf bire gur ynml qbt'" do
-        it "returns 'The quick brown fox jumps over the lazy dog'" do
-            expect(rc.encrypt("Gur dhvpx oebja sbk whzcf bire gur ynml qbt",13)).to eq("The quick brown fox jumps over the lazy dog")
-        end
-    end
+  context "Encrypt with shift key  5 with 'omg'" do
+      it "returns 'trl'" do
+        expect(rc.encrypt("omg",5)).to eq("trl")
+      end
+  end
+  context "Encrypt with shift key  0 with 'c'" do
+      it "returns 'c'" do
+        expect(rc.encrypt("c",0)).to eq("c")
+      end
+  end
+  context "Encrypt with shift key  26 with 'Cool'" do
+      it "returns 'Cool'" do
+        expect(rc.encrypt("Cool",26)).to eq("Cool")
+      end
+  end
+  context "Encrypt with shift key  13 with 'The quick brown fox jumps over the lazy dog'" do
+      it "returns 'Gur dhvpx oebja sbk whzcf bire gur ynml qbt'" do
+        expect(rc.encrypt("The quick brown fox jumps over the lazy dog",13)).to eq("Gur dhvpx oebja sbk whzcf bire gur ynml qbt")
+      end
+  end
+  context "Encrypt with shift key  3 with 'Gur dhvpx oebja sbk whzcf bire gur ynml qbt'" do
+      it "returns 'The quick brown fox jumps over the lazy dog'" do
+        expect(rc.encrypt("Gur dhvpx oebja sbk whzcf bire gur ynml qbt",13)).to eq("The quick brown fox jumps over the lazy dog")
+      end
+  end
 end
