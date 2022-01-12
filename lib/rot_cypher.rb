da7cbbc9c66b7c6880a99d164bf6fe24c5b24a7b
Velimir Gvozdenac <vgvozdenac.vg@gmail.com>
2022-01-12 15:16:10 +0100

    Switch to 2 space ident\ation and reducing rows length

--- lib/rot_cypher.rb
+++ lib/rot_cypher.rb
@@ -1,23 +1,30 @@
 class RotCypher 
-    Letters_array = ("a".."z").to_a
-    Upper_letters_array = ("A".."Z").to_a
+  Letters_array = ("a".."z").to_a
+  Upper_letters_array = ("A".."Z").to_a
 
-    def encrypt(message_to_encrypt, shift_key)
-        encrypted_message = ""
-        message_to_encrypt.split('').each do |a| 
-            if(a == " ")
-                encrypted_message += " "
-            else    
-                if Upper_letters_array.include? a
-                    index = Upper_letters_array.find_index(a)<26-shift_key ? (Upper_letters_array.find_index(a) + shift_key) : (Upper_letters_array.find_index(a)-26+shift_key)
-                    encrypted_message += Upper_letters_array[index]             
-                else
-                    index = Letters_array.find_index(a)<26-shift_key ? (Letters_array.find_index(a) + shift_key) : (Letters_array.find_index(a)-26+shift_key)
-                    encrypted_message += Letters_array[index] 
-                end 
-
-            end
-        end
-        encrypted_message
+  def encrypt(message_to_encrypt, shift_key)
+    encrypted_message = ""
+    message_to_encrypt.split('').each do |letter_to_encrypt| 
+      if(letter_to_encrypt == " ")
+        encrypted_message += " "
+      else  
+        if Upper_letters_array.include? letter_to_encrypt
+          index = if(Upper_letters_array.find_index(letter_to_encrypt)<26-shift_key)
+                    (Upper_letters_array.find_index(letter_to_encrypt) + shift_key)
+                  else
+                    (Upper_letters_array.find_index(letter_to_encrypt)-26+shift_key)
+                  end  
+          encrypted_message += Upper_letters_array[index]
+        else
+          index = if(Letters_array.find_index(letter_to_encrypt)<26-shift_key)
+                    (Letters_array.find_index(letter_to_encrypt) + shift_key)
+                  else
+                    (Letters_array.find_index(letter_to_encrypt)-26+shift_key)
+                  end 
+          encrypted_message += Letters_array[index]
+        end 
+      end
     end
+    encrypted_message
+  end
 end
