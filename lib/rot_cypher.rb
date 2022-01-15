25d319179e8099c52464cfa8632571ff76ca7143
Velimir Gvozdenac <vgvozdenac.vg@gmail.com>
2022-01-15 14:40:17 +0100

    first fission of rot chyper code

--- lib/rot_cypher.rb
+++ lib/rot_cypher.rb
@@ -4,23 +4,43 @@ class RotCypher
 
   def encrypt(message_to_encrypt, shift_key)
     encrypted_message = ""
-    message_to_encrypt.split('').each do |letter_to_encrypt|
-      if(letter_to_encrypt == " ")
+    message_to_encrypt.split('').each do |unencrypted_letter|
+      if unencrypted_letter == " "
         encrypted_message += " "
-      else
-        index_of_unencrypted_letter = UPPER_LETTERS_ARRAY.find_index(letter_to_encrypt.upcase)
-        index_of_encrypted_letter = if(index_of_unencrypted_letter < 26 - shift_key)
-                  (index_of_unencrypted_letter + shift_key)
-                else
-                  (index_of_unencrypted_letter + shift_key - 26)
-                end
-        if UPPER_LETTERS_ARRAY.include? letter_to_encrypt
-          encrypted_message += UPPER_LETTERS_ARRAY[index_of_encrypted_letter]
-        else
-          encrypted_message += LETTERS_ARRAY[index_of_encrypted_letter]
-        end
+        next
       end
+      index_to_encrypt = prepare_encryption_index(unencrypted_letter, shift_key)
+      encrypted_message += letter_encryption(index_to_encrypt, upcase_letter?(unencrypted_letter))
     end
     encrypted_message
+  end
+
+  def letter_encryption(encrypted_letter_index, is_upcase)
+    if is_upcase
+      UPPER_LETTERS_ARRAY[encrypted_letter_index]
+    else
+      LETTERS_ARRAY[encrypted_letter_index]
+    end
+  end
+
+  def prepare_encryption_index(letter_to_encrypt, shift_step)
+    prepared_index_to_encrypt = LETTERS_ARRAY.find_index(letter_to_encrypt.downcase) + shift_step
+    prepared_index_to_encrypt = fix_index_out_of_bounds(prepared_index_to_encrypt)
+  end
+
+  def fix_index_out_of_bounds(index_to_fix)
+    fixed_index_to_encrypt = index_to_fix
+    if index_to_fix >= LETTERS_ARRAY.length()
+      fixed_index_to_encrypt -= LETTERS_ARRAY.length()
+    end
+    fixed_index_to_encrypt
+  end
+
+  def upcase_letter?(letter_to_encrypt)
+    if letter_to_encrypt.upcase == letter_to_encrypt
+      true
+    else
+      false
+    end
   end
 end
