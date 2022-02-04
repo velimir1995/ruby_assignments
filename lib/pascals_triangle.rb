84e0414a367686cf7fff3d4271de4e62d6b9abce
Velimir Gvozdenac <vgvozdenac.vg@gmail.com>
2022-02-04 16:53:32 +0100

    Added to to_s and to_a methods to triangle along with specification and #script

--- lib/pascals_triangle.rb
+++ lib/pascals_triangle.rb
@@ -2,6 +2,10 @@ class PascalsTriangle
 
   STARTING_ROW = [1].freeze
 
+  def initialize(number_of_rows = false)
+    generate_triangle(number_of_rows) if number_of_rows
+  end
+
   def get_row(row_number)
     create_row(row_number)
   end
@@ -15,19 +19,39 @@ class PascalsTriangle
   end
 
   def generate_triangle(number_of_rows)
-    triangle = [STARTING_ROW]
-    ((triangle.length+1)..number_of_rows.to_i).to_a.each do |row_number|
-      triangle.push(create_row(row_number))
+    @triangle = [STARTING_ROW]
+    ((@triangle.length + 1)..number_of_rows.to_i).to_a.each do |row_number|
+      @triangle.push(create_row(row_number))
     end
-    triangle
+    @triangle
+  end
+
+  def to_s
+    get_triangle_as_string if @triangle
+  end
+
+  def to_a
+    @triangle if @triangle
   end
 
   private
 
+  def get_triangle_as_string
+    convert_whole_triangle_to_s(convert_rows_to_s(@triangle))
+  end
+
+  def convert_rows_to_s(triangle)
+    triangle.map { |row| row.join(" ") }
+  end
+
+  def convert_whole_triangle_to_s(triangle)
+    triangle.join("\n")
+  end
+
   def create_row(row_num)
     row  = STARTING_ROW.dup
     (row.length...row_num).to_a.reverse.each do |field_num|
-      field_num == row_num ? row.push(1) : row.push(row.last * field_num / (row_num-field_num))
+      field_num == row_num ? row.push(1) : row.push(row.last * field_num / (row_num - field_num))
     end
     row
   end
--- spec/pascals_triangle_spec.rb
+++ spec/pascals_triangle_spec.rb
@@ -1,21 +1,87 @@
 require "pascals_triangle"
 describe PascalsTriangle do
 
+  describe "#to_s" do
+    let(:pastri) {PascalsTriangle.new}
+    let(:pastri4) {PascalsTriangle.new(4)}
+
+
+    context "With initialized triangle" do
+      it "return String" do
+        expect(pastri4.to_s.instance_of?(String)).to eq(true)
+      end
+    end
+
+    context "Without initialized triangle" do
+      it "return nil" do
+        expect(pastri.to_s).to eq(nil)
+      end
+    end
+
+    context "With initialized triangle to 4" do
+      it "return 1\n1 1\n1 2 1\n1 3 3 1" do
+        expected = "1\n1 1\n1 2 1\n1 3 3 1"
+        expect(pastri4.to_s).to eq(expected)
+      end
+    end
+
+  end
+
+  describe "#to_a" do
+    let(:pastri) {PascalsTriangle.new}
+    let(:pastri5) {PascalsTriangle.new(5)}
+
+    context "With initialized triangle" do
+      it "return Array" do
+        expect(pastri5.to_a.instance_of?(Array)).to eq(true)
+      end
+    end
+
+    context "Without initialized triangle" do
+      it "return nil" do
+        expect(pastri.to_a).to eq(nil)
+      end
+    end
+
+    context "With initialized triangle to 5" do
+      it "return [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]" do
+        expected = [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+        expect(pastri5.to_a).to eq(expected)
+      end
+    end
+
+  end
+
   describe "#generate_triangle" do
     let(:pastri) {PascalsTriangle.new}
+    let(:pastri7) {PascalsTriangle.new(7)}
+    let(:pastri4) {PascalsTriangle.new(4)}
 
-    context "When sent 7, triangle length" do
+    context "When sent 7, length of result" do
       it "return 7" do
         expect(pastri.generate_triangle(7).length).to eq(7)
       end
     end
 
-    context "When sent 5,and try to acces row on index 6" do
+    context "When sent 5,and try to acces row on index 6 of results" do
       it "return nil" do
         expect(pastri.generate_triangle(5)[6]).to eq(nil)
       end
     end
 
+    context "When sent 7 to pastri and then convert to_s" do
+      it "return equal with pastri7.to_s" do
+        pastri.generate_triangle(7)
+        expect(pastri.to_s).to eq(pastri7.to_s)
+      end
+    end
+
+    context "When sent 4 to pastri and then convert to_a" do
+      it "return equal with pastri4.to_a" do
+        pastri.generate_triangle(4)
+        expect(pastri.to_a).to eq(pastri4.to_a)
+      end
+    end
   end
 
   describe "#get_row" do
