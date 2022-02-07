986bb98843c515c7b2e10e0b1960c661e55c1cec
Velimir Gvozdenac <vgvozdenac.vg@gmail.com>
2022-02-07 13:43:52 +0100

    Removed some public interface methods

--- lib/pascals_triangle.rb
+++ lib/pascals_triangle.rb
@@ -3,36 +3,17 @@ class PascalsTriangle
   STARTING_ROW = [1].freeze
 
   def initialize(number_of_rows)
-    @number_of_rows =  number_of_rows
-  end
-
-  def get_row(row_number)
-    create_row(row_number)
-  end
-
-  def get_field(row_index, column_index)
-    create_row(row_index)[column_index]
-  end
-
-  def get_row_sum(row_number)
-    create_row(row_number).inject { |sum, n| sum + n }
+    @number_of_rows = number_of_rows
   end
 
   def to_s
-    if @triangle
-      triangle_as_string
-    else
-      generate_triangle
-      triangle_as_string
-    end
+    generate_triangle unless @triangle
+    triangle_as_string
   end
 
   def to_a
-    if @triangle
-      @triangle
-    else
-      generate_triangle
-    end
+    generate_triangle unless @triangle
+    @triangle
   end
 
   private
@@ -50,7 +31,7 @@ class PascalsTriangle
   end
 
   def create_row(row_num)
-    row  = STARTING_ROW.dup
+    row = STARTING_ROW.dup
     (row.length...row_num).to_a.reverse.each do |field_num|
       field_num == row_num ? row.push(1) : row.push(row.last * field_num / (row_num - field_num))
     end
--- spec/pascals_triangle_spec.rb
+++ spec/pascals_triangle_spec.rb
@@ -4,7 +4,6 @@ describe PascalsTriangle do
   describe "#to_s" do
     let(:pastri4) {PascalsTriangle.new(4)}
 
-
     context "With initialized triangle" do
       it "return String" do
         expect(pastri4.to_s.instance_of?(String)).to eq(true)
@@ -23,7 +22,7 @@ describe PascalsTriangle do
   describe "#to_a" do
     let(:pastri5) {PascalsTriangle.new(5)}
 
-    context "With initialized triangle" do
+    context "With initialized triangle to 5" do
       it "return Array" do
         expect(pastri5.to_a.instance_of?(Array)).to eq(true)
       end
@@ -36,61 +35,6 @@ describe PascalsTriangle do
       end
     end
 
-  end
-
-  describe "#get_row" do
-    let(:pastri6) {PascalsTriangle.new(6)}
-
-    context "When sent 8 " do
-      it "return [1,7,21,35,35,21,7,1]" do
-        expected = [1,7,21,35,35,21,7,1]
-        expect(pastri6.get_row(8)).to eq(expected)
-      end
-    end
-
-    context "When sent 5 and try to access field at index 6" do
-      it "return nil" do
-        expect(pastri6.get_row(5)[6]).to eq(nil)
-      end
-    end
-
-    context "When sent 9, and check field at index 6" do
-      it "return 28" do
-        expect(pastri6.get_row(9)[6]).to eq(28)
-      end
-    end
-  end
-
-  describe "#get_field" do
-    let(:pastri7) {PascalsTriangle.new(7)}
-
-    context "When sent 8 as a row index and 7 as a field index" do
-      it "return 1" do
-        expect(pastri7.get_field(8,7)).to eq(1)
-      end
-    end
-
-    context "When sent 5 as a row index and 7 as a field index" do
-      it "return nil" do
-        expect(pastri7.get_field(5,7)).to eq(nil)
-      end
-    end
-  end
-
-  describe "#get_row_sum" do
-    let(:pastri3) {PascalsTriangle.new(3)}
-
-    context "When sent 8" do
-      it "return 256" do
-        expect(pastri3.get_row_sum(8)).to eq(128)
-      end
-    end
-
-    context "When sent 10" do
-      it "return 1024" do
-        expect(pastri3.get_row_sum(10)).to eq(512)
-      end
-    end
   end
 
 end
