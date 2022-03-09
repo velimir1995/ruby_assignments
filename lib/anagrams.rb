0a7c6ada25b2688f608f6978e27b42a663af32aa
Velimir Gvozdenac <vgvozdenac.vg@gmail.com>
2022-03-09 16:02:10 +0100

    Handling them via hashes

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,7 +1,7 @@
 class Anagram
 
-  def initialize(wordlist)
-    @wordlist = wordlist
+  def initialize(dictionary)
+    @dictionary = dictionary
   end
 
   def to_s
@@ -23,56 +23,35 @@ class Anagram
   end
 
   def search_for_anagrams
-    anagrams = []
-    pairs = populate_pairs
-    if pairs.length > 2
-      anagrams = group_into_anagrams_groups(pairs)
-    elsif pairs.length == 2
-      anagrams = [pairs.transpose[0]] if pairs[0][1] == pairs[1][1]
-    end
-    anagrams.sort_by { |group| group[0] }
-  end
-
-  def populate_pairs
-    pairs = if @wordlist.is_a?(Array) then populate_pairs_from_array
-            elsif File.file?(@wordlist) then populate_pairs_from_file
-            else populate_pairs_from_string
-            end
-    pairs.sort_by { |pair| pair[1] }
-  end
-
-  def populate_pairs_from_array
-    paired_words = []
-    @wordlist.each do |word|
-      paired_words.push([word.strip, word.strip.downcase.chars.sort.join])
-    end
-    paired_words
-  end
-
-  def populate_pairs_from_string
-    @wordlist = @wordlist.split(" ");
-    populate_pairs_from_array
-  end
-
-  def populate_pairs_from_file
-    paired_words = []
-    file = File.open(@wordlist)
-    File.readlines(file).each do |line|
-      paired_words.push([line.strip, line.strip.downcase.chars.sort.join])
-    end
-    paired_words
-  end
-
-  def group_into_anagrams_groups(pairs)
-    anagrams = []
-    pairs.each_with_index do |pair, index|
-      if pair[1] == pairs[index - 2][1]
-        anagrams[anagrams.length - 1].push(pair[0])
-      elsif pair[1] == pairs[index - 1][1]
-        anagrams.push([pairs[index - 1][0], pair[0]])
+    anagrams = {}
+    set_words_to_array(@dictionary).each do |word|
+      sorted_chars = word.strip.downcase.chars.sort.join
+      if anagrams[sorted_chars]
+        anagrams[sorted_chars] << word
+      else
+        anagrams.merge!(sorted_chars => [word])
       end
     end
-    anagrams
+    anagrams.values.delete_if { |words| words.length < 2}
+  end
+
+  def set_words_to_array(dictionary)
+    if dictionary.is_a?(Array)
+      dictionary
+    elsif File.file?(dictionary)
+      populate_words_from_file(dictionary)
+    elsif dictionary.is_a?(String)
+      dictionary.split(" ");
+    end
+  end
+
+  def populate_words_from_file(dictionary)
+    wordslist = []
+    file = File.open(dictionary)
+    File.readlines(file).each do |line|
+      wordslist << line.strip
+    end
+    wordslist
   end
 
 end
