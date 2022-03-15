41c5b804acd814b9e44221cca213bb117924c088
Velimir Gvozdenac <vgvozdenac.vg@gmail.com>
2022-03-15 14:37:58 +0100

    Reorganization and temporary cutting of some specs

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,44 +1,38 @@
 require "anagrams"
 describe Anagram do
 
-  let(:two_words_anagrams) do
-    Anagram.new(Pathname.getwd.to_s + "/spec/support/dictionaries/two_words_anagrams.txt")
+  let(:anagrams_in_pdf) do
+    Anagram.new(File.dirname(__FILE__) + "/support/dictionaries/small_dictionary.pdf")
   end
-  let(:two_words_non_anagrams) do
-    Anagram.new(Pathname.getwd.to_s + "/spec/support/dictionaries/two_words_non_anagrams.txt")
+  let(:no_anagrams_in_txt) do
+    Anagram.new(File.dirname(__FILE__) + "/support/dictionaries/small_dictionary_no_anagrams.txt")
   end
-  let(:small_pdf_dictionary_anagrams) do
-    Anagram.new(Pathname.getwd.to_s + "/spec/support/dictionaries/small_dictionary.pdf")
-  end
-  let(:small_dictionary_without_anagrams) do
-    Anagram.new(Pathname.getwd.to_s + "/spec/support/dictionaries/small_dictionary_no_anagrams.txt")
-  end
-  let(:single_line_file) do
-    Anagram.new(Pathname.getwd.to_s + "/spec/support/dictionaries/single_line.txt")
-  end
-  let(:string_dictonary) { Anagram.new("bup club dub pap pub put tup") }
-  let(:array_dictonary) { Anagram.new(["bup", "club","dub", "pap", "pub", "put", "tup"]) }
+  let(:anagrams_in_two_words_array) { Anagram.new(["spot", "post"]) }
+  let(:no_anagrams_in_one_word_array) { Anagram.new(["non_anagram"]) }
+  let(:anagrams_in_String) { Anagram.new("bup club dub pap pub put tup") }
+  let(:anagrams_in_array) { Anagram.new(["bup", "club","dub", "pap", "pub", "put", "tup"]) }
 
   describe "#find_by_word" do
 
     context "when match" do
-      it "returns group with anagrams in array" do
-        message = "pub"
-        expected = ["bup", "pub"]
-        message1 = "enlist"
-        expected1 = ["enlist", "inlets", "listen", "silent"]
-        expect(string_dictonary.find_by_word(message)).to eq(expected)
-        expect(small_pdf_dictionary_anagrams.find_by_word(message1)).to eq(expected1)
+      it "returns a group of words that are anagram" do
+        message = "enlist"
+        expected = ["enlist", "inlets", "listen", "silent"]
+        expect(anagrams_in_pdf.find_by_word(message)).to eq(expected)
       end
     end
 
     context "when don`t match" do
       it "returns nil" do
         message = "bbbbb"
-        message1 = "enlist"
-        expect(string_dictonary.find_by_word(message)).to eq(nil)
-        expect(small_pdf_dictionary_anagrams.find_by_word(message)).to eq(nil)
-        expect(small_dictionary_without_anagrams.find_by_word(message1)).to eq(nil)
+        expect(anagrams_in_pdf.find_by_word(message)).to eq(nil)
+      end
+    end
+
+    context "when search where is no anagrams" do
+      it "returns nil" do
+        message = "club"
+        expect(no_anagrams_in_txt.find_by_word(message)).to eq(nil)
       end
     end
 
@@ -46,41 +40,21 @@ describe Anagram do
 
   describe "#to_s" do
 
-    context do
-      it "returns a String" do
-        expect(two_words_anagrams.to_s).to be_a(String)
-        expect(two_words_non_anagrams.to_s).to be_a(String)
-        expect(small_pdf_dictionary_anagrams.to_s).to be_a(String)
-        expect(small_dictionary_without_anagrams.to_s).to be_a(String)
-        expect(single_line_file.to_s).to be_a(String)
-        expect(string_dictonary.to_s).to be_a(String)
-        expect(array_dictonary.to_s).to be_a(String)
-      end
+    it "returns a String" do
+      expect(anagrams_in_pdf.to_s).to be_a(String)
     end
 
     context "When there is no anagrams " do
       it "returns an empty String" do
-        expected = ""
-        expect(two_words_non_anagrams.to_s).to eq(expected)
-        expect(single_line_file.to_s).to eq(expected)
-        expect(small_dictionary_without_anagrams.to_s).to eq(expected)
+        expect(no_anagrams_in_txt.to_s).to eq("")
       end
     end
 
-    context "When contain two words that are anagrams " do
-      it "returns one anagram pair" do
-        expected = "enlist silent"
-        expect(two_words_anagrams.to_s).to eq(expected)
-      end
-    end
-
-    context "With wordlist with anagrams " do
-      it "returns an anagrams grouped in lines" do
+    context "When there are anagrams " do
+      it "returns anagrams grouped in lines" do
         expected = "atest State\nboaster boaters borates\ndog God\nenlist inlets listen silent\n"\
                   "fresher refresh\nrots sort\nwoodworm's wormwood's woodworms wormwoods"
-        expected1 = "bup pub\nput tup"
-        expect(small_pdf_dictionary_anagrams.to_s).to eq(expected)
-        expect(array_dictonary.to_s). to eq(expected1)
+        expect(anagrams_in_pdf.to_s).to eq(expected)
       end
     end
 
@@ -88,42 +62,22 @@ describe Anagram do
 
   describe "#to_a" do
 
-    context do
-      it "returns an Array" do
-        expect(two_words_anagrams.to_a).to be_a(Array)
-        expect(two_words_non_anagrams.to_a).to be_a(Array)
-        expect(small_pdf_dictionary_anagrams.to_a).to be_a(Array)
-        expect(small_dictionary_without_anagrams.to_a).to be_a(Array)
-        expect(single_line_file.to_a).to be_a(Array)
-        expect(string_dictonary.to_a).to be_a(Array)
-        expect(array_dictonary.to_a).to be_a(Array)
-      end
+    it "returns an Array" do
+      expect(anagrams_in_pdf.to_a).to be_a(Array)
     end
 
     context "When there is no anagrams in file  " do
       it "returns an empty Array" do
-        expected = []
-        expect(two_words_non_anagrams.to_a).to eq(expected)
-        expect(single_line_file.to_a).to eq(expected)
-        expect(small_dictionary_without_anagrams.to_a).to eq(expected)
+        expect(no_anagrams_in_txt.to_a).to eq([])
       end
     end
 
-    context "When file contain two words that are anagrams " do
-      it "returns one anagram pair" do
-        expected = [["enlist", "silent"]]
-        expect(two_words_anagrams.to_a).to eq(expected)
-      end
-    end
-
-    context "With wordlist with anagrams " do
+    context "When there are anagrams " do
       it "returns anagrams grouped in subarrays" do
         expected = [["atest", "State"], ["boaster", "boaters", "borates"],
                   ["dog", "God"], ["enlist", "inlets", "listen", "silent"],
                   ["fresher", "refresh"], ["rots", "sort"], ["woodworm's", "wormwood's", "woodworms", "wormwoods"]]
-        expected1 = [["bup", "pub"], ["put", "tup"]]
-        expect(small_pdf_dictionary_anagrams.to_a).to eq(expected)
-        expect(string_dictonary.to_a).to eq(expected1)
+        expect(anagrams_in_pdf.to_a).to eq(expected)
       end
     end
 
